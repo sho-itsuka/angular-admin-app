4053fd205b2832ddfaefc7ab975f87a65183da72
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-23 18:48:55 +0900

    fixed members component

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-members',
+  selector:    'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.css']
+  styleUrls:  ['./members.component.css']
 })
 export class MembersComponent implements OnInit {
+
+  member = '田中太郎'
 
   constructor() { }
 
