b77da85c3bec72d56cdf4795dcc32f51b4047ecc
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-23 18:38:51 +0900

    add members component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule
