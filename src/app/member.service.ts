689e48d9b679ddec856b399496a1b5e474ae9367
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-24 16:45:06 +0900

    fixed messages component

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,17 +1,20 @@
 import { Injectable }     from '@angular/core';
 import { Observable, of } from 'rxjs';
 
-import { Member }  from './member';
-import { MEMBERS } from './mock-members';
+import { Member }         from './member';
+import { MEMBERS }        from './mock-members';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
-
-  constructor() { }
-
+  // memberServiceの中でmessageServiceが使えるようにDIする
+  constructor(private messageService: MessageService) { } 
+  
+  // メッセージを追加する
   getMembers(): Observable<Member[]> {
+    this.messageService.add('MemberService: 社員一覧データを取得しました')
     return of(MEMBERS)
   }
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MessageService } from '../message.service';
+
 @Component({
-  selector: 'app-messages',
+  selector:    'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls:  ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  // messageServiceをテンプレートから参照するためにpublic修飾子を利用(publicでないとエラーになる)
+  constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
   }
