894ba1bedf52e48d07fa3c044537604d55c3a035
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-24 19:05:59 +0900

    fixed routing of dashboardcomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { MembersComponent } from './members/members.component';
+import { MembersComponent }   from './members/members.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'members', component: MembersComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'members',   component: MembersComponent }
 ]
 
 @NgModule({
