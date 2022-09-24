3e4729022f8d65666bdd470a1ed05af3b21c2307
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-24 12:40:10 +0900

    added mock-members

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Member } from '../member';
+import { Member }  from '../member';
+import { MEMBERS } from '../mock-members';
 
 @Component({
   selector:    'app-members',
@@ -9,6 +10,8 @@ import { Member } from '../member';
 })
 export class MembersComponent implements OnInit {
 
+  members = MEMBERS
+  
   member: Member = {
     id:   1,
     name: '田中太郎'
