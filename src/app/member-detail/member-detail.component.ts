e51e0dcb337698e8a749cf90a6e18da797e6a781
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-24 21:32:14 +0900

    get id

--- src/app/member-detail/member-detail.component.ts
+++ src/app/member-detail/member-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute }           from '@angular/router';
+import { Location }                 from '@angular/common';
 
 import { Member } from '../member';
+import { MemberService } from '../member.service';
 
 @Component({
   selector:    'app-member-detail',
@@ -8,12 +11,21 @@ import { Member } from '../member';
   styleUrls:  ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-  @Input()
-  member!: Member;
+  @Input() member!: Member;
 
-  constructor() { }
+  constructor(
+    private route:         ActivatedRoute,
+    private memberService: MemberService,
+    private location:      Location
+  ) { }
 
   ngOnInit(): void {
+    this.getMember()
   }
 
+  getMember(): void {
+    const id = +this.route.snapshot.paramMap.get('id')! // 後ろに ! をつけてエラー解消(Object is possibly 'null'.)
+    this.memberService.getMember(id)
+      .subscribe(member => this.member = member)
+  }
 }
--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -11,10 +11,16 @@ import { MessageService } from './message.service';
 export class MemberService {
   // memberServiceの中でmessageServiceが使えるようにDIする
   constructor(private messageService: MessageService) { } 
-  
+
   // メッセージを追加する
   getMembers(): Observable<Member[]> {
     this.messageService.add('MemberService: 社員一覧データを取得しました')
     return of(MEMBERS)
+  }
+
+  // 社員データを取得する
+  getMember(id: number): Observable<Member> {
+    this.messageService.add(`MemberService: 社員データ(id=${id})を取得しました`)
+    return of(MEMBERS.find(member => member.id === id))
   }
 }
