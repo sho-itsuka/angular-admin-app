092537ae104e78a40bb0764f11f7e0a2b1ce4fbf
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-24 19:23:00 +0900

    fixed routing about member.detail.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { MembersComponent }   from './members/members.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { MembersComponent }      from './members/members.component';
+import { DashboardComponent }    from './dashboard/dashboard.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'members',   component: MembersComponent }
+  { path: 'dashboard', component:  DashboardComponent },
+  { path: 'detail/:id', component: MemberDetailComponent },
+  { path: 'members',   component:  MembersComponent }
 ]
 
 @NgModule({
