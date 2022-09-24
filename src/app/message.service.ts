18594185bb228af365dc363e2286ee5d14960802
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-24 16:29:14 +0900

    added add method and clear method

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -5,5 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class MessageService {
 
-  constructor() { }
+  messages: string[] = [] // 初期値は空の配列とする
+
+  add(message: string) { // 文字列でメッセージを受け取る
+    this.messages.push(message)  // messegesプロパティに、受け取ったメッセージを追加する
+  }
+
+  clear() { // 自身のmessagesプロパティをクリアするメソッド
+    this.messages = []
+  }
 }
