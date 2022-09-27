70455d8529512020ce1475b19ff21fa464eee3c7
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-27 23:02:14 +0900

    added http...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
-import { NgModule }         from '@angular/core';
-import { BrowserModule }    from '@angular/platform-browser';
-import { FormsModule }      from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { NgModule }                       from '@angular/core';
+import { BrowserModule }                  from '@angular/platform-browser';
+import { FormsModule }                    from '@angular/forms';
+import { HttpClientModule }               from '@angular/common/http'
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }            from './in-memory-data.service';
 
 import { AppComponent }          from './app.component';
 import { MembersComponent }      from './members/members.component';
@@ -22,6 +24,9 @@ import { DashboardComponent }    from './dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
     AppRoutingModule
   ],
   providers: [],
