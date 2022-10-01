773793a6f0f8a6ca76f9bece1e8dcc025f3c0118
sho-itsuka <sho.itsuka122@gmail.com>
2022-10-01 19:59:07 +0900

    added error handling

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,6 +1,8 @@
 import { Injectable }     from '@angular/core';
 import { HttpClient }     from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+
+import { Observable, of }       from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Member }         from './member';
 import { MEMBERS }        from './mock-members';
@@ -18,8 +20,11 @@ export class MemberService {
   ) { } 
 
   getMembers(): Observable<Member[]> {
-    this.messageService.add('MemberService: 社員一覧データを取得しました')
     return this.http.get<Member[]>(this.membersUrl)
+      .pipe(
+        tap(members => this.log('社員データを取得しました')),
+        catchError(this.handleError<Member[]>('getMembers', []))
+      )
   }
 
   getMember(id: number): Observable<Member | any> {
@@ -29,5 +34,15 @@ export class MemberService {
 
   private log(message: string) {
     this.messageService.add(`MemberService: ${message}`)
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error)
+
+      this.log(`${operation} 失敗: ${error.message}`)
+
+      return of(result as T)
+    }
   }
 }
