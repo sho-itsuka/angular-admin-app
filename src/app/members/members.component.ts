0f45c0cb81e5cf8bb3eed8e20552e8f4ee09f880
sho-itsuka <sho.itsuka122@gmail.com>
2022-10-02 00:07:33 +0900

    added add method

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -24,4 +24,13 @@ export class MembersComponent implements OnInit {
       .subscribe(members => this.members = members)
   }
 
+  add(name: string): void {
+    name = name.trim()
+    if (!name) { return }
+    this.memberService.addMember({ name } as Member)
+      .subscribe((member: Member) => {
+        this.members.push(member)
+      })
+  }
+
 }
